7e3b982375ee348d0360d0e47b37ba6b1d2e7f69
saivenkatavula <saivenkat.avula2020@vitstudent.ac.in>
2025-09-26 20:36:12 +0530

    added cloudinary to the backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 import {connectDB} from "./lib/db.js";
 
 dotenv.config({ path: '../.env.local' });
@@ -15,6 +16,7 @@ app.get('/', (req, res) => {
     res.send("🚀 API is running");
 });
 app.use("/api/auth", authRoutes)
+app.use("/api/message", messageRoutes)
 
 app.listen(PORT, () => {
     console.log('Server is running on port on PORT:', PORT);
