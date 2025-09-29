3041f3b7170298700985d41b357140502e16d35d
saivenkatavula <saivenkat.avula2020@vitstudent.ac.in>
2025-09-30 02:35:53 +0530

    removed the bodyparser

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,8 +13,8 @@ dotenv.config();
 const PORT = process.env.PORT || 5001;
 const __dirname = path.resolve();
 
-app.use(bodyParser.json({ limit: "10mb" }));
-app.use(bodyParser.urlencoded({ limit: "10mb", extended: true }));
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
